5f3a74eafb2082352f7fc89d6d64a782e2bf1e64
Zotoff <paulzotoff@gmail.com>
2023-06-10 12:19:51 +0300

    First attempt

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte a = 1;
+        byte b = 1;
+
+        double formula = ((9 * Math.pow(a, 2)) - (5*b) + 2 + a - 7) * ((a + b - (4*a*b)) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 'c';
+        int h = 'h';
+        int a = 'a';
+        int r = 'r';
+
+        System.out.println(Character.getNumericValue(c));
+        System.out.println(Character.getNumericValue(h));
+        System.out.println(Character.getNumericValue(a));
+        System.out.println(Character.getNumericValue(r));
     }
 }
